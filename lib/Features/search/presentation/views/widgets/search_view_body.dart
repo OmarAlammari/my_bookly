35addb1e5a4942759028044cc61efe757802f10d
OmarAlammari <omaralammari1234@gmail.com>
2023-06-14 00:36:34 +0300

    search view done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:applaid_app_3_my_bookly/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:applaid_app_3_my_bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_search_text_field.dart';
@@ -10,11 +12,39 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
           CustomSearchTextField(),
+          SizedBox(height: 16),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: SearchResultListView(),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+          // child: Text('data'),
+        );
+      },
     );
   }
 }
