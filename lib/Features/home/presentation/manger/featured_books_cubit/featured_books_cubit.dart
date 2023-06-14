3b2085e30fb09202e74049f702e7647a32e4204b
OmarAlammari <omaralammari1234@gmail.com>
2023-06-14 21:45:25 +0300

    create newest books cubit state

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-import 'package:applaid_app_3_my_bookly/Features/home/data/repos/home_repo.dart';
-
 import '../../../data/models/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
